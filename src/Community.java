fe3105257425f21fcbb9d7a280217d413d7eebf7
daoki7 <115014406+daoki7@users.noreply.github.com>
2023-06-20 15:07:24 +0900

    Created Community and CalendarMonth

---
+++ src/Community.java
@@ -0,0 +1,12 @@
+public class Community{
+	private String Com_Name;
+	private String Com_Owner;
+	private String Com_Info;
+	private String Com_Tag[];
+	private String Com_Member[];
+	private CalendarMonth Calendars[];
+	
+	Community(String name, String Owner, String Info, String tag[]){
+		
+	}
+}
