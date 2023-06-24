fff4b13ca6067926bc64b9de93eeca3347386a68
tomitakuto <115090808+tomitakuto@users.noreply.github.com>
2023-06-24 21:21:32 +0900

    メソッドの追加

--- src/ClientEvent.java
+++ src/ClientEvent.java
@@ -1,9 +1,58 @@
 
 public class ClientEvent {
+        String eve_id;
+        String eve_name;
+        String eve_start;
+        String eve_finish;
+        String eve_place;
+        String eve_owner;
+        String eve_outline;
+        String eve_detail;
+        int eve_good;
+        int eve_join;
+        String eve_com_name;
+        String eve_own_message;
+        String eve_cancel;
+        
+        ClientEvent(String name, String start, String finish, String place, String owner, String outline, String detail, String com_name){
+         eve_name = name;
+         eve_start = start;
+         eve_finish = finish;
+         eve_place = place;
+         eve_owner = owner;
+         eve_outline = outline;
+         eve_detail = detail;
+         eve_com_name = com_name;
+    }
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+    //イベント削除//
+    void deleteEvent() {
+    }
 
-	}
+    //イベント編集//
+    void editEvent(String edit) {	
+    }
 
+    //主催者メッセージ送信//
+    void sendMessage(String message) {	
+    }
+    
+    //キャンセル理由送信//
+    void sendCancel(String message) {
+    }
+
+    //いいね数取得//
+    int getGood() {
+        return eve_good;
+    }
+
+    //参加者数取得//
+    int getJoin() {
+        return eve_join;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
