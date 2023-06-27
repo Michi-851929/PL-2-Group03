73422c19f08a15d4be1723e8727a5efd83aad689
zono37 <97681409+zono37@users.noreply.github.com>
2023-06-27 14:48:20 +0900

    ログイン画面のレイアウト作成

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,102 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
-public class Client {
+public class Client extends JFrame {
+	private JTextField usernameField;
+    private JPasswordField passwordField;
 	
-	//コンストラクタ
+	//コンストラクタ(ログイン画面)
 	public Client(){
+		// ウィンドウの設定
+        setTitle("ログイン");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(350, 400);
+        setLocationRelativeTo(null);
+        
+     // コンテンツパネルの設定
+        JPanel contentPane = new JPanel(new GridBagLayout()) {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                // 画像を読み込む
+                Image backgroundImage = new ImageIcon("login.png").getImage();
+                // 画像を描画する
+                g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = GridBagConstraints.RELATIVE;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(10, 5, 5, 5);
+
+        // タイトルラベル
+        JLabel titleLabel = new JLabel("Communi+I", SwingConstants.CENTER);
+        Font titleFont = new Font("Arial", Font.BOLD, 20);
+        titleLabel.setFont(titleFont);
+        contentPane.add(titleLabel, gbc);
+
+        // ユーザ名のラベルとフィールド
+        JLabel usernameLabel = new JLabel("ユーザ名:");
+        usernameField = new JTextField(20);
+        contentPane.add(usernameLabel, gbc);
+        contentPane.add(usernameField, gbc);
+
+        // パスワードのラベルとフィールド
+        JLabel passwordLabel = new JLabel("パスワード:");
+        passwordField = new JPasswordField(20);
+        contentPane.add(passwordLabel, gbc);
+        contentPane.add(passwordField, gbc);
+
+        // ログインボタン
+        JButton loginButton = new JButton("ログイン");
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(10, 0, 0, 0);
+        loginButton.setBackground(new Color(230, 255, 179));
+        contentPane.add(loginButton, gbc);
+
+        // アカウント登録ボタン
+        JButton registerButton = new JButton("アカウント登録");
+        registerButton.setBackground(new Color(230, 255, 179));
+        contentPane.add(registerButton, gbc);
+
+        // ボタンのアクションリスナー
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = new String(passwordField.getPassword());
+                // ログイン処理を行う
+                // ここではダミーの処理として、入力内容を表示するだけとします
+                JOptionPane.showMessageDialog(Client.this, "ユーザ名: " + username + "\nパスワード: " + password);
+            }
+        });
+
+        registerButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // アカウント登録画面に遷移する処理を実装する
+                JOptionPane.showMessageDialog(Client.this, "アカウント登録画面に遷移します");
+            }
+        });
+
+        setContentPane(contentPane);
 		
 	}
 	
@@ -84,7 +177,7 @@ public class Client {
 	//イベント検索
 	String[] serchEvent(String serch_word) {
 		
-		String[] event_list;
+		String[] event_list = null;
 		
 		return event_list;
 	}
@@ -164,7 +257,12 @@ public class Client {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Client loginUI = new Client();
+                loginUI.setVisible(true);
+            }
+        });
 
 	}
 
