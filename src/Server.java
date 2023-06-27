442c3c91ab62bf6160b8db4013025219914bfa71
Michi-851929 <114732502+Michi-851929@users.noreply.github.com>
2023-06-27 19:44:04 +0900

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,17 +1,24 @@
 import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.util.ArrayList;
 import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
-public class Server extends JFrame implements ActionListener{
+public class Server extends JFrame implements ActionListener, FocusListener{
 	private ArrayList<Account> account_list = new ArrayList<>();
 	private ArrayList<Community> community_list = new ArrayList<>();
 	private ArrayList<ClientEvent> event_list = new ArrayList<>();
@@ -23,7 +30,11 @@ public class Server extends JFrame implements ActionListener{
 	private JButton ui_jb_deban;
 	private JButton ui_jb_exit;
 	private TextArea ui_ta_log;
+	private JLabel ui_jl_command;
+	private JTextField ui_tf_text0;
+	private JTextField ui_tf_text1;
 	private JButton ui_jb_commit;
+	private JButton ui_jb_back;
 	
 	public Server()
 	{
@@ -35,7 +46,7 @@ public class Server extends JFrame implements ActionListener{
 		ui_panel_00.setBounds(0, 0, 600 - 15, 400 - 35);
 		
 		ui_panel_01 = new JPanel();
-		ui_panel_01.setLayout(null);
+		ui_panel_01.setLayout(new FlowLayout());
 		ui_panel_01.setBounds(50, 30, 500, 300);
 		ui_panel_01.setVisible(false);
 		ui_panel_00.add(ui_panel_01);
@@ -47,7 +58,6 @@ public class Server extends JFrame implements ActionListener{
 		ui_panel_02.add(ui_jb_make, "A");
 		ui_jb_ban = new JButton("アカウント追放");
 		ui_jb_ban.addActionListener(this);
-		ui_jb_ban.setPreferredSize(new Dimension(150, 50));
 		ui_panel_02.add(ui_jb_ban, "B");
 		ui_jb_deban = new JButton("アカウント復活");
 		ui_jb_deban.addActionListener(this);
@@ -60,6 +70,7 @@ public class Server extends JFrame implements ActionListener{
 		JPanel ui_panel_03 = new JPanel();
 		ui_panel_03.setLayout(new BorderLayout());
 		ui_ta_log = new TextArea("", 100, 100, TextArea.SCROLLBARS_VERTICAL_ONLY);
+		ui_ta_log.setEditable(false);
 		ui_panel_03.add(ui_ta_log);
 		ui_panel_00.add(ui_panel_03, "Center");
 		
@@ -82,8 +93,84 @@ public class Server extends JFrame implements ActionListener{
 		ui_panel_01.removeAll();
 		
 		JPanel ui_panel_04 = new JPanel();
-		ui_panel_04.setLayout(new BorderLayout());
-		
+		ui_panel_04.setLayout(new BorderLayout(64, 64));
+		ui_jl_command = new JLabel(text, JLabel.CENTER);
+		ui_jl_command.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+		ui_panel_04.add(ui_jl_command, "North");
+		JPanel ui_panel_05 = new JPanel();
+		ui_panel_05.setLayout(new GridLayout(2, 1));
+		ui_tf_text0 = new JTextField(24);
+		ui_tf_text0.setText("ユーザ名");
+		ui_tf_text0.setForeground(Color.LIGHT_GRAY);
+		ui_tf_text0.addFocusListener(new FocusListener() {
+			public void focusGained(FocusEvent fe) {
+				String s0 = ui_tf_text0.getText();
+				if (s0.equals("ユーザ名")) {
+					ui_tf_text0.setText("");
+					ui_tf_text0.setForeground(Color.BLACK);
+				}
+			}
+			public void focusLost(FocusEvent fe) {
+				String s0 = ui_tf_text0.getText();
+				if (s0.equals("")) {
+					ui_tf_text0.setText("ユーザ名");
+					ui_tf_text0.setForeground(Color.LIGHT_GRAY);
+				}
+			}
+		});
+		ui_tf_text0.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+		ui_panel_05.add(ui_tf_text0, "A");
+		if(text.equals("アカウント作成")) {
+			ui_tf_text1 = new JTextField(24);
+			ui_tf_text1.setText("パスワード");
+			ui_tf_text1.setForeground(Color.LIGHT_GRAY);
+			ui_tf_text1.addFocusListener(new FocusListener() {
+				public void focusGained(FocusEvent fe) {
+					String s1;
+					if(!(ui_tf_text1 instanceof JPasswordField)) {
+						s1 = ui_tf_text1.getText();
+						if (s1.equals("パスワード")) {
+							System.out.println("gained change");
+							JTextField ui_tf_x = ui_tf_text0;
+							ui_tf_text0 = ui_tf_x;
+							ui_panel_05.removeAll();
+							ui_panel_05.add(ui_tf_text0);
+							ui_tf_text1 = new JPasswordField(24);
+							ui_tf_text1.setText("");
+							ui_tf_text1.setForeground(Color.BLACK);
+							ui_tf_text1.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+							ui_panel_05.add(ui_tf_text1);
+							repaint();
+						}
+					}
+				}
+				public void focusLost(FocusEvent fe) {
+					System.out.println(ui_tf_text1 instanceof JPasswordField);
+					String s1 = new String(((JPasswordField)ui_tf_text1).getPassword());
+					System.out.println(s1);
+					if (s1.equals("")) {
+						System.out.println("lost change");
+						ui_tf_text1 = new JTextField("パスワド");
+						ui_tf_text1.setForeground(Color.LIGHT_GRAY);
+					}
+				}
+			});
+			ui_tf_text1.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+			ui_panel_05.add(ui_tf_text1, "B");
+		}
+		ui_panel_04.add(ui_panel_05, "Center");
+		JPanel ui_panel_06 = new JPanel();
+		ui_panel_06.setLayout(new GridLayout(2, 1, 10, 10));
+		String new_text = String.format("%c%c", text.charAt(5), text.charAt(6));
+		ui_jb_commit = new JButton(new_text);
+		ui_jb_commit.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 16));
+		ui_jb_commit.addActionListener(this);
+		ui_panel_06.add(ui_jb_commit);
+		ui_jb_back = new JButton("戻る");
+		ui_jb_back.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 16));
+		ui_jb_back.addActionListener(this);
+		ui_panel_06.add(ui_jb_back);
+		ui_panel_04.add(ui_panel_06, "South");
 		ui_panel_01.add(ui_panel_04);
 		
 		ui_panel_01.setVisible(true);
@@ -149,13 +236,73 @@ public class Server extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent ae)
 	{
 		String s = ae.getActionCommand();
-		stdout(s);
 		switch(s) {
+		case "作成":
+			createAccount(ui_tf_text0.getText(), new String(((JPasswordField)ui_tf_text1).getPassword()));
+			ui_jb_make.setEnabled(true);
+			ui_jb_ban.setEnabled(true);
+			ui_jb_deban.setEnabled(true);
+			ui_jb_exit.setEnabled(true);
+			ui_panel_01.setVisible(false);
+		case "追放":
+			banAccount(ui_tf_text0.getText());
+			ui_jb_make.setEnabled(true);
+			ui_jb_ban.setEnabled(true);
+			ui_jb_deban.setEnabled(true);
+			ui_jb_exit.setEnabled(true);
+			ui_panel_01.setVisible(false);
+		case "復活":
+			debanAccount(ui_tf_text0.getText());
+			ui_jb_make.setEnabled(true);
+			ui_jb_ban.setEnabled(true);
+			ui_jb_deban.setEnabled(true);
+			ui_jb_exit.setEnabled(true);
+			ui_panel_01.setVisible(false);
+		case "戻る":
+			ui_jb_make.setEnabled(true);
+			ui_jb_ban.setEnabled(true);
+			ui_jb_deban.setEnabled(true);
+			ui_jb_exit.setEnabled(true);
+			ui_panel_01.setVisible(false);
+			break;
 		case "サーバ終了":
 			System.exit(0);
 		default:
 			newWindow(s);
 			break;
+		}
+	}
+	
+	public void focusGained(FocusEvent fe) {
+		String s0 = ui_tf_text0.getText();
+		String s1;
+		if(ui_tf_text1 instanceof JPasswordField) {
+			s1 = new String(((JPasswordField)ui_tf_text1).getPassword());
+		}
+		else {
+			s1 = ui_tf_text1.getText();
+		}
+		if (s0.equals("ユーザ名")) {
+			ui_tf_text0.setText("");
+			ui_tf_text0.setForeground(Color.BLACK);
+		}
+		if (s1.equals("パスワード")) {
+			System.out.println("aaaaa");
+			ui_tf_text1 = new JPasswordField();
+			ui_tf_text1.setForeground(Color.BLACK);
+		}
+	}
+
+	public void focusLost(FocusEvent fe) {
+		String s0 = ui_tf_text0.getText();
+		String s1 = new String(((JPasswordField)ui_tf_text1).getPassword());
+		if (s0.equals("")) {
+			ui_tf_text0.setText("ユーザ名");
+			ui_tf_text0.setForeground(Color.LIGHT_GRAY);
+		}
+		if (s1.equals("")) {
+			ui_tf_text1 = new JTextField("パスワード");
+			ui_tf_text1.setForeground(Color.LIGHT_GRAY);
 		}
 	}
 	
