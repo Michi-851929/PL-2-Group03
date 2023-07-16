b498403d9b18bb7b111f143e1828557fc3e49ebf
ab84ihs <ab84ihs@gmail.com>
2023-07-16 21:11:50 +0900

    Update ConnectName.java

--- src/ConnectName.java
+++ src/ConnectName.java
@@ -1,7 +1,7 @@
 public class ConnectName{
 	//サーバー,クライアント共に使用
-	static final String name = "localhost";
-	static final int port = 8000;
+	static final String name = "ab84ihs.f5.si";
+	static final int port = 443;
 	private ConnectName() {
 		
 	}
