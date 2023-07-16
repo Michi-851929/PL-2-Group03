2b10c00c30d7a06c1320b7d44be9d96bccec02b3
zono37 <97681409+zono37@users.noreply.github.com>
2023-07-16 21:32:08 +0900

    通知画面表示しないように

--- src/Client.java
+++ src/Client.java
@@ -310,7 +310,6 @@ public class Client extends JFrame {
                     if (login_flag == 1) {
                         JOptionPane.showMessageDialog(Client.this, "ログイン成功");
                         login();
-                        notificationScreen();
                     }
                 }
             }
@@ -891,7 +890,7 @@ public class Client extends JFrame {
     void dateScreen() {
 
         try {
-            //event_list.addAll(getADayEvents(ui_ld_looking));
+            event_list.addAll(getADayEvents(ui_ld_looking));
             System.out.println(event_list);
         } catch (Exception e3) {
             // TODO 自動生成された catch ブロック
