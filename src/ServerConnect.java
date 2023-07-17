03906858db1d4042c172f0c877f906ceb6c2d35f
ab84ihs <ab84ihs@gmail.com>
2023-07-18 00:21:08 +0900

    time

--- src/ServerConnect.java
+++ src/ServerConnect.java
@@ -106,9 +106,6 @@ class ConnectThread extends Thread{
             }else if(tmp.verifyPassword(m.pass)==0) {
                 ans.mode = 3;
             }else {
-                if(m.mode != 3) {
-                    tmp.setLastCheckTime();
-                }
                 if(m.mode == 2) {
                     if(se.isCreatableCommunity((String)m.message) != Server.DUPLICATE_NAME) {
                         ans.mode = 4;
