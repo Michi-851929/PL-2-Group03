0890e60d3a6bfc3476dd2a0cc35785f7898d216c
daoki7 <115014406+daoki7@users.noreply.github.com>
2023-07-17 13:26:35 +0900

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -21,6 +21,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.image.BufferedImage;
