e002b8c1d067dc49105cc4d51688d1f310298131
ab84ihs <ab84ihs@gmail.com>
2023-07-20 10:39:21 +0900

    競合の解消

--- src/ConnectName.java
+++ src/ConnectName.java
@@ -1,6 +1,6 @@
 public class ConnectName{
 	//サーバー,クライアント共に使用
-	static final String name = "localhost";
+	static final String name = "192.168.1.3";
 	static final int port = 8000;
 	private ConnectName() {
 		
