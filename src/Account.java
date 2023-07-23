b87fe22243df48158497a7af294a974ec11ae14c
Michi-851929 <114732502+Michi-851929@users.noreply.github.com>
2023-07-23 22:16:38 +0900

    Update Account.java

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,7 @@
 import java.io.Serializable;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -14,18 +17,27 @@ public class Account implements Serializable{
     private ArrayList<String> event_going = new ArrayList<>();
     private ArrayList<String> event_preferred = new ArrayList<>();
     private ArrayList<String> event_made = new ArrayList<>();
+    public final String SALT_FRONT = "f784ge8fiawurfheshrtawt8uwt8h4yd";
+    public final String SALT_BACK = "u8ofhaeioniqahriawrfjarfaifbdurg";
     int today_eventnum=0;
     //パスワード認証の戻り値を表す定数
     public final static int PASS_CORRECT = 1; //正しい
     public final static int PASS_FALSE = 0; //正しくない
-    
+
     private Calendar today = Calendar.getInstance();
 
     //コンストラクタ
     public Account(String name, String password, String mac_adress)
     {
         this.name = name;
-        this.password = password;
+        MessageDigest sha256 = null;
+        try {
+            sha256 = MessageDigest.getInstance("SHA-256");
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        this.password = String.format("%40x", new BigInteger(1, sha256.digest((SALT_FRONT + password + SALT_BACK).getBytes())));
         this.mac_adress = mac_adress;
         is_banned = false;
         lastcheck[0] = LocalDateTime.now().getYear();
@@ -73,9 +85,9 @@ public class Account implements Serializable{
 
         return time;
     }
-    
+
     public int[] getLastCheckInt(){
-    	return lastcheck;
+        return lastcheck;
     }
 
     public void setLastCheckTime()
@@ -93,6 +105,17 @@ public class Account implements Serializable{
     //正しいとき(int)PASS_CORECT, 正しくないとき(int)PASS_FALSEを返す
     public int verifyPassword(String password)
     {
+        if(password==null) {
+            return PASS_FALSE;
+        }
+        MessageDigest sha256 = null;
+        try {
+            sha256 = MessageDigest.getInstance("SHA-256");
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        password = String.format("%40x", new BigInteger(1, sha256.digest((SALT_FRONT + password + SALT_BACK).getBytes())));
         if(password.equals(this.password)) {
             return PASS_CORRECT;
         }
@@ -108,7 +131,14 @@ public class Account implements Serializable{
     {
         int pass_result = verifyPassword(old_password);
         if(pass_result == PASS_CORRECT) {
-            password = new_password;
+            MessageDigest sha256 = null;
+            try {
+                sha256 = MessageDigest.getInstance("SHA-256");
+            }
+            catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+            password = String.format("%40x", new BigInteger(1, sha256.digest((SALT_FRONT + new_password + SALT_BACK).getBytes())));
         }
         return pass_result;
     }
@@ -128,7 +158,12 @@ public class Account implements Serializable{
     //所属コミュニティを取得
     public String[] getCommunity()
     {
-        return (String[])community_involved.toArray(new String[community_involved.size()]);
+        if(!community_involved.isEmpty()){
+            return (String[])community_involved.toArray(new String[community_involved.size()]);
+        }else {
+            String[] tmp = {};
+            return tmp;
+        }
     }
 
     //参加イベントを追加
@@ -166,42 +201,46 @@ public class Account implements Serializable{
     {
         return event_preferred;
     }
-    
+
     //あるイベントに参加しているか
     public boolean getAEventGoing(String s) {
-    	for(String str: event_going) {
-    		if(str.equals(s)) {
-    			return true;
-    		}
-    	}
-    	return false;
+        for(String str: event_going) {
+            if(str.equals(s)) {
+                return true;
+            }
+        }
+        return false;
     }
-    
+
     //あるイベントにいいねしているか
     public boolean getAEventPreferrd(String s) {
-    	for(String str: event_preferred) {
-    		if(str.equals(s)) {
-    			return true;
-    		}
-    	}
-    	return false;
+        for(String str: event_preferred) {
+            if(str.equals(s)) {
+                return true;
+            }
+        }
+        return false;
     }
-    
+
     //作成したイベントを追加
     public void addEventMade(String event) {
-    	event_made.add(event);
+        event_made.add(event);
     }
     //作成したイベントを取得
     public ArrayList<String> getEventMade()
     {
         return event_made;
     }
-    
+
     //作成したイベントの総数を取得
     public int getTotalEventMade() {
-    	return event_made.size();
+        if(event_made!=null) {
+            return event_made.size();
+        }else {
+            return 0;
+        }
     }
-    
+
     public boolean getEventMakeSig(Server se) //いいね数からイベント作成できるかを判断
     {
         ResourceBundle rb = ResourceBundle.getBundle("Border");
